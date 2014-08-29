7667776871e283e9754378b44e8f737e7b72534d
vxhex <tabete@gmail.com>
2014-08-29 13:44:51 -0500

    Adding network request logging

--- gmail_checker.rb
+++ gmail_checker.rb
@@ -3,6 +3,8 @@ require 'gmail'
 File.readlines('users.txt').each do |user_pass|
     user, pass = user_pass.split(' ')
     gmail = Gmail.new(user, pass)
+
+    # Check for messages
     if gmail.inbox.count(:unread, :from => 'hit-reply@linkedin.com') > 0
         file = File.open('log.txt', 'a')
         `notify-send -t 5000 "New recruiter blacklist emails are available!"`
@@ -13,10 +15,32 @@ File.readlines('users.txt').each do |user_pass|
             file.write("#{email.subject}\n")
             body = email.body.to_s
             body.each_line do |line|
-                file.write("https://www.linkedin.com/profile/view?id=#{$1}\n\n") if line =~ /\/fpg\/(\d{6,12})\/(?:.*) LinkedIn profile/
+                # TODO pretty sure this is broken now
+                file.write("https://www.linkedin.com/profile/view?id=#{$1}\n") if line =~ /\/fpg\/(\d{6,12})\/(?:.*) LinkedIn profile/
             end
+            file.write("\n")
         end
-        file.close unless file == nil
+        file.close unless file.nil?
     end
+
+    # Check for networking requests
+    if gmail.inbox.count(:unread, :from => 'member@linkedin.com') > 0
+        file = File.open('log.txt', 'a')
+        `notify-send -t 5000 "New recruiter blacklist emails are available!"`
+        gmail.inbox.emails(:unread, :from => 'member@linkedin.com').each do |email|
+            email.mark(:read)
+            file.write("#{user}\n")
+            file.write("#{email.from.first[:name]}\n")
+            file.write("#{email.subject}\n")
+            body = email.body.to_s
+            body.each_line do |line|
+                file.write("#{line.strip}\n") if line =~ /has indicated you/
+                file.write("https://www.linkedin.com/profile/view?id=#{$1}\n") if line =~ /\/fpg\/(\d{6,12})\/(?:.*) LinkedIn profile/
+            end
+            file.write("\n")
+        end
+        file.close unless file.nil?
+    end
+
     gmail.logout
 end
